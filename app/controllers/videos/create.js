92e3d1a26994218436d2cafefc941e0656c4188d
David Dang <dtudang@gmail.com>
2015-02-26 18:07:52 -0800

    updated create controller action

--- app/controllers/videos/create.js
+++ app/controllers/videos/create.js
@@ -1,13 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  var _this = this,
-  actions:{
-    create: function(){
+  // model:function(){
 
-      console.clear();
-      var playlist = this.get("newPlaylistName");
-
+    //   console.clear();
+    //   var playlist = this.get("newPlaylistName");
+    //   return this.store.createRecord('playlist', {name: playlist});
+    // },
+    actions: {
+      create: function(model) {
+        console.clear();
+        var _this = this;
+        model.save().then(function() {
+        _this.transitionToRoute('videos.search', model)
+        console.log(model);
+      });
     }
   }
 });
--- app/routes/videos/create.js
+++ app/routes/videos/create.js
@@ -2,16 +2,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
- model: function(){
-   var playlist = this.modelFor('playlists/show')
-   return this.store.createRecord('video', {playlist: playlist});
- },
- actions: {
-   addVideo: function(video) {
-     // var playlistController = this.get('controllers.playlists.show')
-     var playlist = this.modelFor('playlists/show')
-     var song = this.store.createRecord('song', {playlist: playlist, name: video.title, url: video.url})
-     song.save()
-   }
- }
+ // model: function(){
+ //   var playlist = this.modelFor('playlists/show')
+ //   return this.store.createRecord('video', {playlist: playlist});
+ // },
+ // actions: {
+ //   addVideo: function(video) {
+ //     // var playlistController = this.get('controllers.playlists.show')
+ //     var playlist = this.modelFor('playlists/show')
+ //     var song = this.store.createRecord('song', {playlist: playlist, name: video.title, url: video.url})
+ //     song.save()
+ //   }
+ // }
 });
