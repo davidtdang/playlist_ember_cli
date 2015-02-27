061f947073cff64876e4a36597dbb442e661ef7f
David Dang <dtudang@gmail.com>
2015-02-27 10:05:36 -0800

    latest working build

--- app/controllers/playlists/create.js
+++ app/controllers/playlists/create.js
@@ -1,20 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // model:function(){
-
-    //   console.clear();
-    //   var playlist = this.get("newPlaylistName");
-    //   return this.store.createRecord('playlist', {name: playlist});
-    // },
-    actions: {
-      create: function(model) {
-        console.clear();
-        var _this = this;
-        model.save().then(function() {
-        _this.transitionToRoute('videos.search', model)
-        console.log(model);
+  actions: {
+    create: function(){
+    console.log("clicked");
+      var playlist = this.get("newPlaylistName");
+      return this.store.createRecord('playlist', {name: playlist});
+      var _this = this;
+      playlist.save().then(function(){
+        _this.transitionToRoute('videos.search', playlist);
       });
+
     }
   }
+
 });
--- app/models/video.js
+++ app/models/video.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  author: DS.attr('string'),
-  link: DS.attr('string'),
-  uid: DS.attr('string'),
+  description: DS.attr('string'),
+  thumbnail: DS.attr('string'),
+  videoId: DS.attr('string'),
   playlist: DS.belongsTo('playlist')
 });
